e4a9511b305ff910ba9c70fa21fb05930ed0b521
Myles Reyburn <mylesreyburn@gmail.com>
2023-04-10 00:03:13 -0600

    done

---
+++ server/migrations/versions/6efdac28da1b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6efdac28da1b
+Revises: 3f56860cbb31
+Create Date: 2023-04-09 22:24:56.039456
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6efdac28da1b'
+down_revision = '3f56860cbb31'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -2,6 +2,21 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+# BAKERIES HAVE:
+# "baked_goods"
+# "created_at"
+# "id"
+# "name"
+# "updated_at"
+
+# BAKED GOODS HAVE:
+# "bakery_id"
+# "created_at"
+# "id"
+# "name"
+# "price"
+# "updated_at"
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -11,10 +26,24 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ('-baked_goods.bakery',)
+
+    baked_goods = db.relationship('BakedGood', backref = 'bakery')
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.baked_goods',)
+
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
     
